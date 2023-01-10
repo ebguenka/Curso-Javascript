f34ae5985b17456dcde4ccc585a3549feb1f31d2
ebguenka <eduardoguenka@hotmail.com>
2023-01-10 20:53:29 -0300

    CursoJavaScript

---
+++ aula62/index.js
@@ -0,0 +1,13 @@
+//Valor por referência
+
+//                 0          1       2
+const nomes = ['Bruno', 'Marta', 'Monica'];
+const removido = nomes.pop();
+console.log(nomes, removido);
+// const novo = [...nomes];
+// novo.pop();
+//nomes[2] = 'João';
+//delete nomes[2];
+
+// console.log(nomes);
+// console.log(novo);
