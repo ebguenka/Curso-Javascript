46de5d44e63825c08bd4ebaf3527976482a08003
ebguenka <eduardoguenka@hotmail.com>
2023-01-11 11:40:12 -0300

    CursoJavaScript

--- aula63/index.js
+++ aula63/index.js
@@ -4,7 +4,19 @@ const nomes = ['Maria', 'Ana', 'Janaina', 'Rita', "Jair"];
 const removidos = nomes.splice(3, 0, 'Ronaldo');
 console.log(nomes, removidos);
 
+// Unshift elemento no começo do indice
+//nomes.splice(0, 0, 'Eder', 'Tati');
 
+//*******************************************
+// Push Adicionar elemento
+//nomes.splice(nome.length, 0, 'Eder');
+//*******************************************
+//pop remover o último elemento
+// const removidos = nomes.splice(-1, 1);
+
+//*******************************************
+//shift remover o primeiro elemento
+//const removidos = nomes.splice(0, 1);
 
 //*******************************************
 // const removidos = nomes.splice(-2, Number.MAX_VALUE);
