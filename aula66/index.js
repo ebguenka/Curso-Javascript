68c163ead8ade3db27355e44cf59d021af382093
ebguenka <eduardoguenka@hotmail.com>
2023-01-13 09:24:01 -0300

    CursoJavaScript

---
+++ aula66/index.js
@@ -0,0 +1,9 @@
+// Dobre os números
+
+const numeros = [5, 50, 80, 1, 2, 3, 4, 5, 6, 7, 11, 15, 22, 27];
+const numerosEmDobro = numeros.map(function(valor) {
+   return valor * 2;
+    // return`{Eduardo -> ${valor}`;
+
+});
+console.log(numerosEmDobro);
