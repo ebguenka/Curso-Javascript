db07fbb28722ddd4ebe807f27d7ffd87bcba6d7f
ebguenka <eduardoguenka@hotmail.com>
2023-01-24 12:08:34 -0300

    Herança com classes

---
+++ aula82/index.js
@@ -0,0 +1,54 @@
+class DispositivoEletronico {
+    constructor(nome) {
+        this.nome = nome;
+        this.ligado = false;
+    }
+
+    ligar() {
+        if(this.ligado) {
+            console.log(this.nome + ' já ligado');
+            return;
+        }
+
+        this.ligado = true;
+    }
+
+    desligar() {
+        if(!this.ligado) {
+            console.log(this.nome + ' já desligado');
+            return;
+        }
+        this.ligado = false;
+
+    }
+}
+// const d1 = new DispositivoEletronico('Smartphone');
+// d1.ligar();
+// d1.desligar();
+// console.log(d1);
+class Smartphone extends DispositivoEletronico {
+constructor(nome, cor, modelo) {
+    super(nome);
+    this.cor = cor;
+    this.modelo = modelo;
+   }   
+}
+class Tablet extends DispositivoEletronico {
+    constructor(nome, temWifi) {
+        super(nome);
+        this.temWifi = temWifi;
+
+    }
+
+    ligar() {
+        console.log('Olha, você alterou o método ligar.');
+    }
+}
+
+const s1 = new Smartphone('LG', 'Preto', 'K7');
+s1.desligar();
+console.log(s1);
+
+const t1 = new Tablet('IPad', true);
+t1.ligar();
+console.log(t1);
